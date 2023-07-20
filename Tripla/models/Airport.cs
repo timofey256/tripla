9324072ae110af3a89d84458df456b845ec33457
schetilt <shetilin.timofey@gmail.com>
2023-07-20 14:10:14 +0200

    Added format of Airport.at property since it's defined as string

--- Tripla/models/Airport.cs
+++ Tripla/models/Airport.cs
@@ -4,4 +4,5 @@ public class Airport {
 	public string iataCode { get; set; }
 	public string terminal { get; set; }
 	public string at { get; set; }
+	public const string atFormat = "s"; // "sortable" type of DateTime formatting: yyyy-MM-ddTHH-mm-ss
 }
