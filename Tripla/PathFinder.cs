8d597021de8895a583a386572bbdbecd0d805378
schetilt <shetilin.timofey@gmail.com>
2023-07-21 23:56:32 +0200

    Created a new class to build a path through *multiple* cities 1/2

---
+++ Tripla/PathFinder.cs
@@ -0,0 +1,13 @@
+namespace Tripla;
+
+class PathFinder {	
+	private readonly List<Airport> points; 
+	
+	public PathFinder(List<Airport> _points) {
+		points = _points;	
+	}
+
+	public List<FlightOffer> BuildItinerary() {
+	
+	}
+}
