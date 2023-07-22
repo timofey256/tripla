7cab249127c8c517bc2e0ae00044f5bd58d22072
schetilt <shetilin.timofey@gmail.com>
2023-07-22 21:22:48 +0200

    Added function to build an itinerary through *multiple* cities 2/2

--- Tripla/PathFinder.cs
+++ Tripla/PathFinder.cs
@@ -1,13 +1,32 @@
 namespace Tripla;
 
 class PathFinder {	
-	private readonly List<Airport> points; 
+	private readonly AmadeusApiClient client;	
 	
-	public PathFinder(List<Airport> _points) {
-		points = _points;	
+	public PathFinder(string apiKey, string apiSecret) {
+		client = new AmadeusApiClient(apiKey, apiSecret, 1);
 	}
 
-	public List<FlightOffer> BuildItinerary() {
-	
+	public async Task<FlightOfferResponse> BuildItinerary(List<Airport> points) {
+		List<FlightOffer> flights = new List<FlightOffer>();
+
+		for (int i=0; i < points.Count-1; i++) {
+			var cur = points[i];
+			var next = points[i+1];
+
+			string origin = cur.iataCode;
+		       	string destination = next.iataCode;
+			DateTime date = DateTime.Parse(cur.at);
+
+			FlightOfferResponse response = await client.GetFlights(origin, destination, date);	
+			FlightOffer offer = response.data[0];
+			flights.Add(offer);	
+		}
+		
+		var itinerary = new FlightOfferResponse() {
+			data = flights
+		};
+		
+		return itinerary;
 	}
 }
