24c850dfc46399e47912981c98e6e3d3d39872dd
timothy256 <shetilin.timofey@gmail.com>
2023-08-09 23:51:16 +0200

    Added starter script for asp.net

--- Tripla/Program.cs
+++ Tripla/Program.cs
@@ -1,23 +1,35 @@
 ﻿using System.Net.Http; 
 using System.Text.RegularExpressions;
 
-namespace SkyScannerScraper;
+namespace Tripla;
 
 class Program {
-	public static void Main(string[] args) {
-		var formatter = new UrlFormatter();
-		formatter.startDate = "230724";
-		formatter.endDate = "230730";
-		formatter.startCity = "Prague";
-		string skyScannerUrl = formatter.getUrl(true);
-		Console.WriteLine($"Generated URL: {skyScannerUrl}");
-		//string page = Scrapper.GetHTML(skyScannerUrl);
-		//Console.WriteLine($"Page: {page} \n-------------");
-		string page = Scrapper.GetPregeneratedHTML(@"./pregeneratedPage.html");
-		PageParser parser = new PageParser(page);
-		var nodes = parser.GetAllCities();
-		foreach (var city in nodes) {
-			Console.WriteLine($"Country: {city.Name}, \nPrice: {city.Price} \n Link: {city.Link}. \n");
+	private static void BuildApp(string[] args) {
+		var builder = WebApplication.CreateBuilder(args);
+	
+		builder.Services.AddControllersWithViews();
+
+		var app = builder.Build();
+		app.UseHttpLogging();
+
+		if (!app.Environment.IsDevelopment()) {
+			app.UseExceptionHandler("/Home/Error");
+			app.UseHsts();
 		}
+		
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.UseAuthorization();
+
+		app.MapGet("/", () => "Hello World!");
+
+		app.Run();
+	}
+	
+	public static void Main(string[] args) {
+		BuildApp(args);
 	}
 }
