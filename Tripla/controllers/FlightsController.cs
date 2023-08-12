f585edb978d3c2486d01e3d98dd8682d75580ef7
schetilt <shetilin.timofey@gmail.com>
2023-08-12 16:17:47 +0200

    Added GET endpoint to retrieve tickets for several flights at once

--- Tripla/controllers/FlightsController.cs
+++ Tripla/controllers/FlightsController.cs
@@ -9,18 +9,36 @@ public class FlightsController : ControllerBase {
 	private readonly AmadeusApiClient client; 
 	
 	public FlightsController() {
-		client = new AmadeusApiClient(Credentials.apiKey, Credentials.apiSecret, 1); // Credentials.cs is an uncommited file where senstitive data is stored
+		// Credentials.cs is an uncommited file where senstitive data is stored
+		client = new AmadeusApiClient(Credentials.apiKey, Credentials.apiSecret, 1); 
 	}
 
-	[HttpGet]
+	[HttpGet("single")]
 	public async Task<IActionResult> Get([FromQuery] string dateString, [FromQuery] string origin, [FromQuery] string dest) {
-        if (!DateTime.TryParse(dateString, out DateTime date))
-        {
-            return BadRequest("Bad date format");
-        }
+		if (!DateTime.TryParse(dateString, out DateTime date))
+		{
+		    return BadRequest("Bad date format");
+		}
 
 		var flights = await client.GetFlights(origin, dest, date);
-		
+			
 		return Ok(flights);
+	}
+
+	[HttpGet("route")]
+	public async Task<IActionResult> Get([FromQuery] List<FlightInfo> route) {
+		var itinerary = new List<FlightOfferResponse>();
+
+		foreach (var point in route) {
+			if (!DateTime.TryParse(point.DateString, out DateTime date))
+			{
+			    return BadRequest("Bad date format");
+			}
+
+			var flightTicket = await client.GetFlights(point.Origin, point.Dest, date);
+			itinerary.Add(flightTicket);
+		}
+
+		return Ok(itinerary);
 	}
 }
